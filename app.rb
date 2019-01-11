138288669cc8651eb43320029b47d2b93bc25777
Certil Remy <certil@noukod.com>
2019-01-11 15:24:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get '/' do
       erb :index
+    end
+
+    get'/display_puppy' do
+      erb display_puppy
     end
 
 end
